ab7bfa7b21acbc2370be6dd81c53a735f5989691
taskdaily45@gmail.com <taskdaily45@gmail.com>
2016-05-19 18:05:14 +0500

    practice in typescript daily

---
+++ BasicType/basicType.js
@@ -0,0 +1,10 @@
+// Boolean
+var isDone = false;
+//Number
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+//String
+var color = "blue";
+name = 'red';
---
+++ BasicType/basicType.ts
@@ -0,0 +1,14 @@
+
+// Boolean
+let isDone: boolean = false;
+
+//Number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+
+//String
+let color: string = "blue";
+name = 'red';
